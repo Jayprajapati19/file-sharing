edea451d17a2a99134ef4e04dd4c09d2289caa9b
JayPrajapati19 <jayp37679@gmail.com>
2024-06-28 19:22:11 +0530

    perview error

--- app/(dashboard)/(routes)/upload/_compponents/FilePreview.jsx
+++ app/(dashboard)/(routes)/upload/_compponents/FilePreview.jsx
@@ -1,8 +1,15 @@
+import Image from 'next/image'
 import React from 'react'
 
-function FilePreview() {
+function FilePreview({ file }) {
     return (
-        <div>FilePreview</div>
+        <div className='flex items-center '>
+            <Image src='/file.png' width={50} height={50} alt='file' />
+            <div className='text-gray-900 text-left gap-3'>
+                <h2>{file.name}</h2>
+                <h2 className='text-[12px] text-gray-600'>{file?.type} / {(file.size / 1024 / 1024).toFixed(2)}MB</h2>
+            </div>
+        </div>
     )
 }
 
--- app/(dashboard)/(routes)/upload/_compponents/UploadForm.jsx
+++ app/(dashboard)/(routes)/upload/_compponents/UploadForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import AlertMsg from './AlertMsg';
+import FilePreview from './FilePreview';
 
 function UploadForm() {
     const [file, setFile] = useState();
@@ -38,6 +39,7 @@ function UploadForm() {
             </div>
 
             {errorMsg ? < AlertMsg msg={errorMsg} /> : null}
+            {file ? <FilePreview file={file} /> : null}
             <button button disabled={!file} className='p-2 bg-primary text-white w-[30%] rounded-full mt-5 disabled:bg-gray-400 '>Upload</button>
 
         </div >
