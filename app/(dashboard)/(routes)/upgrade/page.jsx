3bb946f7935fa97adaee4a0cd205c2ac4879b87f
JayPrajapati19 <jayp37679@gmail.com>
2024-06-29 23:34:51 +0530

    errror solving

--- app/(dashboard)/(routes)/upgrade/page.jsx
+++ app/(dashboard)/(routes)/upgrade/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Upgrade() {
     return (
         <div className="mx-auto max-w-3xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
-            <h1></h1>
+            <h1>Currently not working this section</h1>
             <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 sm:items-center md:gap-8">
                 <div
                     className="rounded-2xl border border-indigo-600 p-6 shadow-sm ring-1 ring-indigo-600 sm:order-last sm:px-8 lg:p-12"
